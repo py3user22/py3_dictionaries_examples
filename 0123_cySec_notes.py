95b12baa84d05ffa7d9f8fe22567ae3385fac9ee
Jesus G <113757515+py3user22@users.noreply.github.com>
2023-02-02 21:32:25 -0600

    Add files via upload
    updated some descriptions and put some links for more info.

---
+++ 0123_cySec_notes.py
@@ -0,0 +1,85 @@
+#!/bin/python3
+
+
+"""Dictionary for cyberSecurity"""
+
+cySec_notes = {
+    "CIS": 'Center for Internet Security',
+    "NIST": 'National Institute Security Technology',
+    "GDPR": 'General data protection regulations',
+    "HIPAA": 'Health Insurance portability & accountability act',
+    "PCI-DSS": 'Payment card industry - Data standard security',
+    "ISO": 'International org for standardization',
+    "AICPA": 'American Institute of CPA',
+    "SOC": 'System & organization controls',
+    "MITRE_ATT&CK": ' ',
+    "ACSC": 'Australian Cyber Security Center',
+    "SIEM": 'Security Incident & event management system',
+    "NCSC": 'UK National CySec Center',
+    "NAT": 'Network address translation',
+    "SQL": 'Structured Query language',
+    "": '',
+
+
+}
+
+for kee, valu in cySec_notes.items():
+    print("{} > {}".format(kee, valu))
+
+
+
+ex>0127> MITRE | ATT&CK
+-----------------------
+https://attack.mitre.org/mitigations/M1026/
+
+
+------------------------------------------------------------------
+
+
+ex>0127> ACSC 
+-------------
+https://www.cyber.gov.au/acsc/view-all-content/publications/secure-administration
+
+https://www.cyber.gov.au/acsc/view-all-content/ism
+
+
+------------------------------------------------------------------
+
+
+rapid modernization plan (RAMP)
+-------------------------------
+will help you quickly adopt Microsoft's recommended privileged access strategy.
+
+https://learn.microsoft.com/en-us/security/compass/security-rapid-modernization-plan
+
+
+------------------------------------------------------------------
+
+
+SIEM (Security information and event management) is a solution that helps org's detect, analyze, and
+respond to security threats before they harm business operations.
+ 
+SIEM, pronounced “sim,” combines both security information management (SIM) and 
+security event management (SEM) into one security management system. 
+SIEM technology collects event log data from a range of sources, identifies activity that deviates from
+the norm with real-time analysis, and takes appropriate action
+--------------------
+
+https://www.microsoft.com/en-us/security/business/security-101/what-is-siem
+
+
+------------------------------------------------------------------
+
+
+CIS Controls Microsoft Windows 10 Cyber Hygiene Guide
+------------------------------------------------------
+You learned about the Center for Internet Security  (CIS) Controls in Module 1 of this course.
+Explore a CIS control for Microsoft Windows 10 in detail by downloading this white-paper from CIS:
+
+
+https://www.cisecurity.org/white-papers/cis-controls-microsoft-windows-10-cyber-hygiene-guide/
+
+
+------------------------------------------------------------------
+
+
